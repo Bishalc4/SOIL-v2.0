d422827ea274c0b3dee757f0085989d18a250520
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-14 18:16:52 +1000

    Uncommented model imports

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -13,24 +13,24 @@ db.sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
 
 // Include models.
 db.user = require("./models/user.js")(db.sequelize, DataTypes);
-// db.admin = require("./models/admin.js")(db.sequelize, DataTypes);
-// db.cart = require("./models/cart.js")(db.sequelize, DataTypes);
-// db.following = require("./models/following.js")(db.sequelize, DataTypes);
-// db.product = require("./models/product.js")(db.sequelize, DataTypes);
-// db.review = require("./models/review.js")(db.sequelize, DataTypes);
-// db.special = require("./models/special.js")(db.sequelize, DataTypes);
+db.admin = require("./models/admin.js")(db.sequelize, DataTypes);
+db.cart = require("./models/cart.js")(db.sequelize, DataTypes);
+db.following = require("./models/following.js")(db.sequelize, DataTypes);
+db.product = require("./models/product.js")(db.sequelize, DataTypes);
+db.review = require("./models/review.js")(db.sequelize, DataTypes);
+db.special = require("./models/special.js")(db.sequelize, DataTypes);
 
 //establish connections
-// db.user.hasOne(db.cart);
-// db.cart.belongsTo(db.user, { forgiegnKey: { name: "username", allowNull: false}});
-// db.user.hasOne(db.admin);
-// db.admin.belongsTo(db.user, { foreignKey: "username", allowNull: false });
-// db.user.hasMany(db.review);
-// db.review.belongsTo(db.user, { foreignKey: "username" });
-// db.product.hasMany(db.review);
-// db.review.belongsTo(db.product, { foreignKey: "producy_id" });
-// db.product.hasOne(db.special_deal);
-// db.special_deal.belongsTo(db.product, { foreignKey: 'Product_id' });
+db.user.hasOne(db.cart);
+db.cart.belongsTo(db.user, { forgiegnKey: { name: "username", allowNull: false}});
+db.user.hasOne(db.admin);
+db.admin.belongsTo(db.user, { foreignKey: "username", allowNull: false });
+db.user.hasMany(db.review);
+db.review.belongsTo(db.user, { foreignKey: "username" });
+db.product.hasMany(db.review);
+db.review.belongsTo(db.product, { foreignKey: "producy_id" });
+db.product.hasOne(db.special_deal);
+db.special_deal.belongsTo(db.product, { foreignKey: 'Product_id' });
 
 db.sync = async () => {
   // Sync schema.
