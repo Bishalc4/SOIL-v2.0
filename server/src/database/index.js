2ee694b9f631444b5d684db294d408acb7c247c0
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-14 17:55:42 +1000

    Added sync functoin and seed data in database

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -13,32 +13,47 @@ db.sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
 
 // Include models.
 db.user = require("./models/user.js")(db.sequelize, DataTypes);
+// db.admin = require("./models/admin.js")(db.sequelize, DataTypes);
+// db.cart = require("./models/cart.js")(db.sequelize, DataTypes);
+// db.following = require("./models/following.js")(db.sequelize, DataTypes);
+// db.product = require("./models/product.js")(db.sequelize, DataTypes);
+// db.review = require("./models/review.js")(db.sequelize, DataTypes);
+// db.special = require("./models/special.js")(db.sequelize, DataTypes);
+
+//establish connections
+// db.user.hasOne(db.cart);
+// db.cart.belongsTo(db.user, { forgiegnKey: { name: "username", allowNull: false}});
+// db.user.hasOne(db.admin);
+// db.admin.belongsTo(db.user, { foreignKey: "username", allowNull: false });
+// db.user.hasMany(db.review);
+// db.review.belongsTo(db.user, { foreignKey: "username" });
+// db.product.hasMany(db.review);
+// db.review.belongsTo(db.product, { foreignKey: "producy_id" });
+// db.product.hasOne(db.special_deal);
+// db.special_deal.belongsTo(db.product, { foreignKey: 'Product_id' });
 
 db.sync = async () => {
   // Sync schema.
   await db.sequelize.sync();
 
-  //seed function can be placed here
+  await seedData();
 };
 
-db.admin = require("./models/admin.js")(db.sequelize, DataTypes);
-db.cart = require("./models/cart.js")(db.sequelize, DataTypes);
-db.following = require("./models/following.js")(db.sequelize, DataTypes);
-db.product = require("./models/product.js")(db.sequelize, DataTypes);
-db.review = require("./models/review.js")(db.sequelize, DataTypes);
-db.special = require("./models/special.js")(db.sequelize, DataTypes);
+async function seedData() {
+  const count = await db.user.count();
+
+  if (count > 0) {
+    return;
+  }
+
+  const argon2 = require("argon2");
+
+  let hash = await argon2.hash("abc123", { type: argon2.argon2id });
+
+  await db.user.create({ username: "mbolger", password_hash: hash, email: "mbolger@gmail.com", first_name: "Matthew", last_name : "Bolger", joinDate: "2024-05-14 12:34:56"});
+}
 
 
 
-db.user.hasOne(db.cart);
-db.cart.belongsTo(db.user, { forgiegnKey: { name: "username", allowNull: false}});
-db.user.hasOne(db.admin);
-db.admin.belongsTo(db.user, { foreignKey: "username", allowNull: false });
-db.user.hasMany(db.review);
-db.review.belongsTo(db.user, { foreignKey: "username" });
-db.product.hasMany(db.review);
-db.review.belongsTo(db.product, { foreignKey: "producy_id" });
-db.product.hasOne(db.special_deal);
-db.special_deal.belongsTo(db.product, { foreignKey: 'Product_id' });
 
 module.exports = db;
