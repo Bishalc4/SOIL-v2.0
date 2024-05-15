1cc0460dc183f992ad67a2bd404f70c58fc6f805
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-15 21:20:03 +1000

    added following model

--- server/src/database/models/following.js
+++ server/src/database/models/following.js
@@ -1,7 +1,22 @@
-module.exports = (sequelize, DataTypes) =>
-    sequelize.define("following", {
-        
-    }, {
-      // Don't add the timestamp attributes (updatedAt, createdAt).
-      timestamps: true
-    });  
+module.exports = (sequelize, DataTypes) => {
+  sequelize.define("following", {
+    username: {
+      type: DataTypes.STRING(254),
+      primaryKey: true,
+      references: {
+        model: sequelize.models.user,
+        key: "username"
+      }
+    },
+    following_username: {
+      type: DataTypes.STRING(254),
+      primaryKey: true,
+      references: {
+        model: sequelize.models.user, 
+        key: "username"
+      }
+    }
+  }, {
+    timestamps: false
+  });
+};
