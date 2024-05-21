f7ad0c07468591572146aa2d3da9ee7c83fa899d
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-21 20:00:01 +1000

    implemented product controller

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -1,15 +1,40 @@
 const db = require("../database");
 
-// Select all products from the database.
-exports.all = async (req, res) => {
-    const products = await db.product.findAll();
-  
+//Select one product from the database
+exports.one = async (req, res) => {
+  try {
+    const product = await db.product.findByPk(req.params.productID, {
+      include: ["special"],
+    });
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+}
+
+//Get all products with their special prices (null otherwise)
+exports.findAll = async (req, res) => {
+    const products = await db.product.findAll({
+        include: ["special"],
+      });
+
     res.json(products);
 };
 
-// Select one product from the database.
-exports.one = async (req, res) => {
-    const product = await db.product.findByPk(req.params.id);
-  
-    res.json(product);
+//Get all special products (only special products)
+exports.allSpecials = async (req, res) => {
+  const specials = await db.product.findAll({
+      include: {
+        model: db.special,
+        as: "special",
+        required: true,
+      },
+    });
+
+  res.json(specials);
 };
