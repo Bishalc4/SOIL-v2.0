fbeee10a60f8cf4724f258ac61d62508c4714de8
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-22 13:43:58 +1000

    Implemented browsing by category from backend

--- client/src/Pages/Browse/Browse.jsx
+++ client/src/Pages/Browse/Browse.jsx
@@ -1,18 +1,32 @@
 import { useState, useEffect } from 'react';
-import { findAllProducts } from '../../data/product';
+import { findProductsByCategory } from '../../data/product';
 import ProductCard from '../../Components/Layout/ProductCard/ProductCard';
 import images from '../../assets';
 import "./Browse.scss"
 
 function Browse() {
     const [isLoading, setIsLoading] = useState(true);
-    const [products, setProducts] = useState([]);
+
+    const [fruits, setFruits] = useState([]);
+    const [vegetables, setVegetables] = useState([]);
+    const [dairy, setDairy] = useState([]);
+    const [meat, setMeat] = useState([]);
+    const [grains, setGrains] = useState([]);
 
     const fetchProducts = () => {
         async function loadTasks() {
-            const fetchedProducts = await findAllProducts();
+            const fetchedFruits = await findProductsByCategory('Fruits');
+            const fetchedVegetables = await findProductsByCategory('Vegetables');
+            const fetchedDairy = await findProductsByCategory('Dairy');
+            const fetchedMeat = await findProductsByCategory('Meat');
+            const fetchedGrains = await findProductsByCategory('Grains');
 
-            setProducts(fetchedProducts);
+            setFruits(fetchedFruits);
+            setVegetables(fetchedVegetables);
+            setDairy(fetchedDairy);
+            setMeat(fetchedMeat);
+            setGrains(fetchedGrains);
+
             setIsLoading(false);
         }
 
@@ -27,10 +41,47 @@ function Browse() {
         <div className="browse-container">
             <h1>Browse products by category</h1>
             <div className="products-grid-container">
-               {isLoading ? (
+                <h2>Fruits</h2>
+                {isLoading ? (
                     <p>Loading...</p>
                 ) : (
-                    products
+                    fruits
+                        .map((item, index) => (
+                            <ProductCard key={index} product={item} image={images[item.imageUrl]} />
+                        ))
+                )}
+                <h2>Vegetables</h2>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    vegetables
+                        .map((item, index) => (
+                            <ProductCard key={index} product={item} image={images[item.imageUrl]} />
+                        ))
+                )}
+                <h2>Dairy</h2>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    dairy
+                        .map((item, index) => (
+                            <ProductCard key={index} product={item} image={images[item.imageUrl]} />
+                        ))
+                )}
+                <h2>Meat</h2>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    meat
+                        .map((item, index) => (
+                            <ProductCard key={index} product={item} image={images[item.imageUrl]} />
+                        ))
+                )}
+                <h2>Grains</h2>
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    grains
                         .map((item, index) => (
                             <ProductCard key={index} product={item} image={images[item.imageUrl]} />
                         ))
