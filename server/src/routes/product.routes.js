fee6fb8da81ade14660a1d9deff736cfb997331e
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-22 12:11:58 +1000

    refactored product routes for better readability

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -2,15 +2,20 @@ module.exports = (express, app) => {
   const controller = require("../controllers/product.controller.js");
   const router = express.Router();
 
+  //get all products
   router.get("/", controller.findAll);
 
+  //get all special products
+  router.get("/specials", controller.allSpecials)
+
+  //retrieve a singular product (specified by productID)
   router.get("/select/:productID", controller.one);
 
-  router.get("/category/product/:productCategory", controller.findByCategory);
+  //get all products of a certain category (specified by productCategory)
+  router.get("/category/:productCategory", controller.findByCategory);
 
-  router.get("/category/specials/:productCategory", controller.findBySpecialsCategory);
-
-  router.get("/specials", controller.allSpecials)
+  //get all special products of a certain category (specified by productCategory)
+  router.get("/category/:productCategory/specials", controller.findBySpecialsCategory);
 
   // Add routes to server.
   app.use("/api/products", router);
