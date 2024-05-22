51519980c39ea5cc84db2cedc003d8d089d139c8
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-23 01:37:04 +1000

    Added review route

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
 //Add routes
 require("./src/routes/user.routes.js")(express, app);
 require("./src/routes/product.routes.js")(express, app);
+require("./src/routes/review.routes.js")(express, app);
 
 // Set port, listen for requests.
 const PORT = 4000;
