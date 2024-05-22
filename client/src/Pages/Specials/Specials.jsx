9fe54511ab0fdc8a8e6f05c69bb2c40851dbb23f
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-22 13:24:13 +1000

    Implemented specials data fetching from API

--- client/src/Pages/Specials/Specials.jsx
+++ client/src/Pages/Specials/Specials.jsx
@@ -1,27 +1,42 @@
 import { useState, useEffect } from 'react';
+import { findAllSpecials } from '../../data/product';
+
 import ProductCard from "../../Components/Layout/ProductCard/ProductCard";
 import images from "../../assets";
 import "./Specials.scss";
 
 //Special deals page - displays products on sale
 function Specials(){
-    const [product, setProduct] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [specials, setSpecials] = useState([]);
+
+    const fetchSpecials = () => {
+        async function loadTasks() {
+            const specials = await findAllSpecials();
+
+            setSpecials(specials);
+            setIsLoading(false);
+            console.log(specials);
+        }
+
+        loadTasks();
+    };
 
     useEffect(() => {
-        const storedProduct = JSON.parse(localStorage.getItem("products"));
-        if (storedProduct) {
-            setProduct(storedProduct);
-        }
+        fetchSpecials();
     }, []);
 
     return(
         <div className="specials-container">
             <div className="products-grid-container">
-                {product
-                    .filter(item => item.specialPrice !== null) //if productName contains the query string
-                    .map((item, index) => (
-                    <ProductCard key={index} product={item} image={images[item.imageUrl]} />
-                ))}
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    specials
+                        .map((item, index) => (
+                            <ProductCard key={index} product={item} image={images[item.imageUrl]} />
+                        ))
+                )}
             </div>
         </div>
     );
