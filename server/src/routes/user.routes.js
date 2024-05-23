cae1ec5113ec94004086269f1de8cc8a452ce670
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-24 00:47:26 +1000

    added comments to user controller

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -2,13 +2,15 @@ module.exports = (express, app) => {
     const controller = require("../controllers/user.controller.js");
     const router = express.Router();
   
-    router.get("/", controller.all);
+    router.get("/", controller.all);  // select all users.
 
-    router.get("/select/:id", controller.one);
+    router.get("/select/:username", controller.one);  // Select a single user with id.
 
-    router.get("/login", controller.login);
+    router.get("/login", controller.login);      // Select one user from the database if username and password are a match.
 
-    router.post("/", controller.create);
+    router.post("/", controller.create);        //  Create a new user.
+
+    router.delete("/delete/:username", controller.delete);      // Delete a user with username
   
     // Add routes to server.
     app.use("/api/users", router);
