46c5ee9c05b60be1b9dc8f6bd01886d77d674c76
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-24 00:46:59 +1000

    added routes for deleting a user

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const db = require("../database");
 const argon2 = require("argon2");
 
+// Select all users from the database.
 exports.all = async (req, res) => {
     const users = await db.user.findAll();
   
@@ -9,11 +10,29 @@ exports.all = async (req, res) => {
 
 //select one user from the database
 exports.one = async (req, res) => {
-    const user = await db.user.findByPk(req.params.id);
+    const user = await db.user.findByPk(req.params.username);
   
     res.json(user);
 };
 
+// Delete a user with the specified username in the request
+exports.delete = async (req, res) => {
+  try {
+      const username = req.params.username;
+      const user = await db.user.findByPk(username);
+
+      if (user) {
+          await user.destroy();
+          res.status(200).json({ message: `User with username ${username} deleted successfully.` });
+      } else {
+          res.status(404).json({ message: `User with username ${username} not found.` });
+      }
+  } catch (error) {
+      res.status(500).json({ message: "An error occurred while trying to delete the user."});
+  }
+};
+
+// Select one user from the database if username and password are a match.
 exports.login = async (req, res) => {
     const user = await db.user.findByPk(req.query.username);
   
