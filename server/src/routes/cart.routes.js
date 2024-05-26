3e2bab312053db9292f64c6579ae546e7c6fe51c
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-26 17:13:29 +1000

    adding routes for handling cart items

--- server/src/routes/cart.routes.js
+++ server/src/routes/cart.routes.js
@@ -9,6 +9,14 @@ module.exports = (express, app) => {
     router.post("/", controller.create);        //  Create a cart for the user that register.
 
     router.delete("/delete/:id", controller.delete);      // Delete a cart with id
+
+    router.get("/items", controller.allItem);  // select all the carts items
+
+    router.get("/items/select/:cart_id/:product_id", controller.oneItem);  // select a single cart items with id 
+
+    router.post("/items", controller.createItem);  // create a cart items
+    
+    router.delete("/items/delete/:cart_id/:product_id", controller.deleteItem);  // Delete a cart items with id
   
     // Add routes to server.
     app.use("/api/carts", router);
