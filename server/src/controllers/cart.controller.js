d1eb55cfeddb47342d8f026a9e0715cc4a07cfe1
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-26 17:12:54 +1000

    added method for handling cart items

--- server/src/controllers/cart.controller.js
+++ server/src/controllers/cart.controller.js
@@ -30,11 +30,47 @@ exports.delete = async (req, res) => {
   
         if (cart) {
             await cart.destroy();
-            res.status(200).json({ message: `cart with id ${id} deleted successfully.` });
+            res.status(200).json({ message: `cart with cartid ${id} deleted successfully.` });
         } else {
-            res.status(404).json({ message: `cart with id ${id} not found.` });
+            res.status(404).json({ message: `cart with cartid ${id} not found.` });
         }
     } catch (error) {
         res.status(500).json({ message: "An error occurred while trying to delete the cart."});
     }
+  };
+
+  
+  exports.allItem = async (req, res) => {
+    const carts_item = await db.cart_item.findAll();
+    res.json(carts_item);
+  };
+
+  exports.createItem = async (req, res) => {
+    const cart_item = await db.cart_item.create({
+      cart_id: req.body.cart_id,
+      product_id: req.body.product_id,
+      quantity: req.body.quantity
+    });
+  
+    res.json(cart_item);
+  };
+
+  exports.oneItem = async (req, res) => {
+    const cartItem = await db.cart_item.findOne({ where: { cart_id: req.params.cart_id, product_id: req.params.product_id } });
+    if (cartItem) {
+        res.json(cartItem);
+    } else {
+        res.status(404).json({ message: `Cart item with cart ID ${req.params.cart_id} and product ID ${req.params.product_id} not found.` });
+    }
+  };
+
+  exports.deleteItem = async (req, res) => {
+    const cartItem = await db.cart_item.findOne({ where: { cart_id: req.params.cart_id, product_id: req.params.product_id } });
+
+        if (cartItem) {
+            await cartItem.destroy();
+            res.status(200).json({ message: `Cart item with cart ID ${cart_id} and product ID ${product_id} deleted successfully.` });
+        } else {
+            res.status(404).json({ message: `Cart item with cart ID ${cart_id} and product ID ${product_id} not found.` });
+        }
   };
