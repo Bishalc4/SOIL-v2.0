617509a36cabd9b80551e97a5af166baebdfbaf7
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-27 15:27:04 +1000

    Added a review form component

---
+++ client/src/Components/Layout/ReviewForm/ReviewForm.jsx
@@ -0,0 +1,12 @@
+import "./ReviewForm.scss"
+
+function ReviewForm() {
+    return (
+        <div className="review-form-container">
+            
+            <p>Review form goes here</p>
+        </div>
+    );
+}
+
+export default ReviewForm;
