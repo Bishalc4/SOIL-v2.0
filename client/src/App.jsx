5dba6464a40524e0846d51d27b04a302d01a4c63
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-30 01:35:01 +1000

    removed proudctlist function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,11 @@ import { useLocation } from "react-router-dom"
 import Header from './Components/Layout/Header/Header.jsx'
 import Navbar from './Components/Layout/Navbar/Navbar.jsx'
 import Footer from './Components/Layout/Footer/Footer.jsx'
-import ProductsList from "./Functions/ProductsList.js"
 import AllRoutes from "./Pages/AllRoutes.jsx"
 
 import './App.css'
 
 function App() {
-  ProductsList();
   
   const location = useLocation();
   const isLoginPage = (location.pathname === '/login' || location.pathname === '/signup');
