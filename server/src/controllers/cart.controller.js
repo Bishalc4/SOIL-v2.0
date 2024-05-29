69d20a289c1c3a6d957a131dcd79ee209687baa6
Bishalc4 <s4009432@student.rmit.edu.au>
2024-05-29 14:52:40 +1000

    fixed deleting all items related to single user

--- server/src/controllers/cart.controller.js
+++ server/src/controllers/cart.controller.js
@@ -18,11 +18,10 @@ exports.create = async (req, res) => {
 //select one cart from the database
 exports.one = async (req, res) => {
     const cart = await db.cart.findByPk(req.params.id);
-  
     res.json(cart);
 };
 
-// Delete a cart with the specified id in the request
+// delete a cart with the specified id in the request
 exports.delete = async (req, res) => {
     try {
         const id = req.params.id;
@@ -39,12 +38,13 @@ exports.delete = async (req, res) => {
     }
   };
 
-  
+  // select all carts with their item from database
   exports.allItem = async (req, res) => {
     const carts_item = await db.cart_item.findAll();
     res.json(carts_item);
   };
 
+  // create a cart item in the database
   exports.createItem = async (req, res) => {
     const cart_item = await db.cart_item.create({
       cart_id: req.body.cart_id,
@@ -55,6 +55,18 @@ exports.delete = async (req, res) => {
     res.json(cart_item);
   };
 
+  // select a cart with the specified cartid in the request
+  exports.oneCart = async (req, res) => {
+    const cartItem = await db.cart_item.findAll({ where: { cart_id: req.params.cart_id } });
+    if (cartItem) {
+        res.json(cartItem);
+    } else {
+        res.json(null);
+        res.status(404).json({ message: `Cart item with cart ID ${req.params.cart_id}` });
+    }
+  };
+
+  // select a cart with the specified cartid and productid in the request
   exports.oneItem = async (req, res) => {
     const cartItem = await db.cart_item.findOne({ where: { cart_id: req.params.cart_id, product_id: req.params.product_id } });
     if (cartItem) {
@@ -64,13 +76,47 @@ exports.delete = async (req, res) => {
     }
   };
 
+  // delete a cart with the specified cartid in the request
+  exports.deleteCart = async (req, res) => {
+    const cartItems = await db.cart_item.findAll({ where: { cart_id: req.params.cart_id } });
+
+        if (cartItems) {
+        for (const cartItem of cartItems) {
+            await cartItem.destroy();
+        }
+        res.status(200).json({ message: `Cart items with cart ID ${req.params.cart_id} deleted successfully.` });
+    } else {
+        res.status(404).json({ message: `Cart items with cart ID ${req.params.cart_id} not found.` });
+    }
+  };
+
+  // delete a cart with the specified cartid and productid in the request
   exports.deleteItem = async (req, res) => {
     const cartItem = await db.cart_item.findOne({ where: { cart_id: req.params.cart_id, product_id: req.params.product_id } });
 
+    if (cartItem) {
+        await cartItem.destroy();
+        res.status(200).json({ message: `Cart item with cart ID ${req.params.cart_id} and product ID ${req.params.product_id} deleted successfully.` });
+    } else {
+        res.status(404).json({ message: `Cart item with cart ID ${req.params.cart_id} and product ID ${req.params.product_id} not found.` });
+    }
+};
+
+  exports.updateQuantity = async (req, res) => {
+    try {
+        const { cart_id, product_id } = req.params;
+        const { quantity } = req.body;
+
+        const cartItem = await db.cart_item.findOne({ where: { cart_id, product_id } });
+
         if (cartItem) {
-            await cartItem.destroy();
-            res.status(200).json({ message: `Cart item with cart ID ${cart_id} and product ID ${product_id} deleted successfully.` });
+            cartItem.quantity = quantity;
+            await cartItem.save();
+            res.json(cartItem);
         } else {
             res.status(404).json({ message: `Cart item with cart ID ${cart_id} and product ID ${product_id} not found.` });
         }
-  };
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred while updating quantity." });
+    }
+};
--- server/src/routes/cart.routes.js
+++ server/src/routes/cart.routes.js
@@ -1,23 +1,29 @@
 module.exports = (express, app) => {
-    const controller = require("../controllers/cart.controller.js");
-    const router = express.Router();
+  const controller = require("../controllers/cart.controller.js");
+  const router = express.Router();
+
+  router.get("/", controller.all);  // select all carts.
+
+  router.get("/select/:id", controller.one);  // Select a single cart with id.
+
+  router.post("/", controller.create);        //  Create a cart for the user that register.
+
+  router.delete("/delete/:id", controller.delete);      // Delete a cart with id
+
+  router.get("/items", controller.allItem);  // select all the carts items
+
+  router.get("/items/select/:cart_id/:product_id", controller.oneItem);  // select a single item with cartid and productid
+
+  router.get("/items/select/:cart_id", controller.oneCart);  // select all cart items with cartid
+
+  router.post("/items", controller.createItem);  // create a cart item
   
-    router.get("/", controller.all);  // select all carts.
+  router.delete("/items/delete/:cart_id/:product_id", controller.deleteItem);  // Delete a cart item with cartid and productid
 
-    router.get("/select/:id", controller.one);  // Select a single cart with id.
+  router.delete("/items/delete/:cart_id", controller.deleteCart);  // delete a cart with cartid
 
-    router.post("/", controller.create);        //  Create a cart for the user that register.
+  router.put("/items/:cart_id/:product_id", controller.updateQuantity); 
 
-    router.delete("/delete/:id", controller.delete);      // Delete a cart with id
-
-    router.get("/items", controller.allItem);  // select all the carts items
-
-    router.get("/items/select/:cart_id/:product_id", controller.oneItem);  // select a single cart items with id 
-
-    router.post("/items", controller.createItem);  // create a cart items
-    
-    router.delete("/items/delete/:cart_id/:product_id", controller.deleteItem);  // Delete a cart items with id
-  
-    // Add routes to server.
-    app.use("/api/carts", router);
-  };
+  // Add routes to server.
+  app.use("/api/carts", router);
+};
