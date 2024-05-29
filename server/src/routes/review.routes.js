41cbf206e710d70d65eaaa75eddc1144e6c84ed2
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-29 14:22:21 +1000

    Updated product routes for new functions

--- server/src/routes/review.routes.js
+++ server/src/routes/review.routes.js
@@ -5,14 +5,26 @@ module.exports = (express, app) => {
   //get all reviews
   router.get("/", controller.findAll);
 
+  //create a review
+  router.post("/", controller.createReview)
+   
+  //update a Review with id
+  router.put("/:id", controller.update);
+
+  //delete a Review with id
+  router.delete("/:id", controller.delete);
+
   //get all reviews for a particular product
   router.get("/product/:product_id", controller.findForProduct);
+
+  //delete a Review with product_id
+  router.delete("product/:product_id", controller.deleteProductReviews);
 
   //get all reviews for a particular user
   router.get("/user/:username", controller.findByUser);
 
-  //create a review
-  router.post("/", controller.createReview)
+  //delete a Review with username
+  router.delete("user/:username", controller.deleteUserReviews);
 
   // Add routes to server.
   app.use("/api/reviews", router);
