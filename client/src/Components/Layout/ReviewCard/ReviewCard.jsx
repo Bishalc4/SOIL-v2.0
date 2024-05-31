3b60a5e9e1901fc55c82d5cb061f8591ee47b4d2
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-05-31 22:28:33 +1000

    Delete review pop up logic

--- client/src/Components/Layout/ReviewCard/ReviewCard.jsx
+++ client/src/Components/Layout/ReviewCard/ReviewCard.jsx
@@ -1,18 +1,35 @@
 import ProfileAvatar from "../../../assets/ProfileAvatar.png";
 import { FaStar } from "react-icons/fa";
 import { FaRegStar } from "react-icons/fa";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { ImBin } from "react-icons/im";
+import ConfirmDeletePopUp from "../ConfirmDeletePopUp/ConfirmDeletePopUp";
+import EditReviewPopUp from "../EditReviewPopUp/EditReviewPopUp";
 import "./ReviewCard.scss"
+import { useState } from 'react';
 
 function ReviewCard(props) {
+    const [showDeletePopup, setShowDeletePopUp] = useState(false);
+    const [showEditPopUp, setShowEditPopUp] = useState(false);
+
+    const handleDeleteBtnClick = () => {
+        setShowDeletePopUp(true);
+    };
+
+    const handleEditBtnClick = () => {
+        setShowEditPopUp(true);
+    }
 
     var date = props.review.updatedAt;
     const year = date.substring(0, 4);
     const month = date.substring(5, 7);
     const day = date.substring(8, 10)
     date = day+"/"+month+"/"+year;
+
+    const currUser = JSON.parse((localStorage.getItem("user")));
     
     const text = props.review.text;
-    const username = props.review.username;
+    const username = props.review.username;username
 
     const stars = [];
     const rating = props.review.rating;
@@ -25,6 +42,9 @@ function ReviewCard(props) {
         }
     }
 
+    //if currUser == username, also display a delete button and edit button
+    //if updatetime !== createTime -> have "(edited)" next to the username
+
     return(
         <div className="review-card-container">
             <div className="profile-image-container">
@@ -34,10 +54,43 @@ function ReviewCard(props) {
                 <div className="star-rating-container">
                     {stars}
                 </div>
-                <h2>@{username}</h2>
+                <div className="username-btns-container">
+                    <h2>@{username}</h2>
+                    {currUser === username ? (
+                        <>
+                            <div className="edit-delete-container">
+                                <button id="editBtn" onClick={handleEditBtnClick}>
+                                    <div className="edit-container">
+                                        <MdOutlineModeEdit />
+                                        <span>Edit</span>
+                                    </div>
+                                </button>
+                                <button id="deleteBtn" onClick={handleDeleteBtnClick}>
+                                    <div className="delete-container">
+                                        <ImBin className="bin-icon"/>
+                                        <span>Delete</span>
+                                    </div>
+                                </button>
+                            </div>
+                        </>
+                        ): ( <></> )
+                    }
+                </div>
                 <p>{text}</p>
                 <p className="review-date">{date}</p>
             </div>
+            {showDeletePopup && (
+                <div>
+                    <div className="review-overlay" onClick={() => setShowDeletePopUp(false)}></div>
+                    <ConfirmDeletePopUp onClose={() => setShowDeletePopUp(false)} id={props.review.review_id}/>
+                </div>
+            )}
+            {showEditPopUp && (
+                <div>
+                    <div className="review-overlay" onClick={() => setShowEditPopUp(false)}></div>
+                    <EditReviewPopUp onClose={() => setShowEditPopUp(false)} id={props.review.review_id}/>
+                </div>
+            )}
         </div>
     );
 }
