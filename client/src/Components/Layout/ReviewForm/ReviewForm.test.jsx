c59a092a919d0199913e765ac47862627eb887e2
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-06-02 10:00:42 +1000

    Review test implementation

--- client/src/Components/Layout/ReviewForm/ReviewForm.test.jsx
+++ client/src/Components/Layout/ReviewForm/ReviewForm.test.jsx
@@ -1,16 +1,45 @@
-import { describe, expect, vi } from "vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { beforeEach, describe, expect, vi, afterEach } from "vitest";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import ReviewForm from "./ReviewForm";
+import { createReview } from "../../../data/review";
+
+vi.mock('../../../data/review', () => ({
+    createReview: vi.fn(),
+}));
 
 describe("ReviewForm", () => {
-    it("submit a review with valid text and rating", () => {
+    beforeEach(() => {
+        localStorage.setItem("user", JSON.stringify("mbolger"));
+    });
 
-        const updateParentState = vi.fn; //don't know what this is doing
+    afterEach(() => {
+        localStorage.clear();
+    });
+
+    it("submit a review with text and rating", async () => {
+        const updateParentState = vi.fn(); //don't know what this is doing
 
         render(<ReviewForm updateParentState={updateParentState} product_id={3}/>)
 
-        // fireEvent.change(screen.getByPlaceholderText(/Share your thoughts about this product here.../i), {
-        //     target: { value: 'This fruit was kinda mid ngl.'}
-        // });
+        fireEvent.change(screen.getByPlaceholderText(/Share your thoughts about this product here.../i), {
+            target: { value: 'This fruit was kinda mid ngl.'},
+        });
+
+        const stars = screen.getAllByTestId(/^star-\d$/);
+
+        fireEvent.click(stars[4]);
+
+        const button = screen.getByRole("button");
+
+        fireEvent.click(button);
+
+        await waitFor(() => {
+            expect(createReview).toHaveBeenCalledWith({
+                text: 'This fruit was kinda mid ngl.',
+                rating: 5,
+                username: 'mbolger',
+                product_id: 3
+            });
+        });
     });
 });
