0eab092528ca404978336194d8ac5d76cf6a8819
PannyaTrehan <s4003909@student.rmit.edu.au>
2024-06-02 16:41:48 +1000

    Structure for dashboard page

--- admin-front-end/src/Components/Navbar/Navbar.jsx
+++ admin-front-end/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import { GrUploadOption } from "react-icons/gr";
 import "./Navbar.scss"
 
 function Navbar() {
     return(
         <div className="navbar-container">
+            <button className="create-product-button">
+                <GrUploadOption className="upload-icon" />
+                <span>Create product</span>
+            </button>
+            <div className="profile-icon">
 
+            </div>
         </div>
     );
 }
--- admin-front-end/src/Pages/Dashboard/Dashboard.jsx
+++ admin-front-end/src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,36 @@
 import "./Dashboard.scss"
 
 function Dashboard() {
+
+    //product sales container will have the graph that real time updates and shows the number of products purchased
+    //last row will simply contain the latest cutomers reviews
+
     return(
         <div className="dashboard-container">
-            <p>Dashboard page</p>
+            <div className="row" id="sales-container">
+                <div className="total-sales-container">
+
+                </div>
+                <div className="total-orders-container">
+
+                </div>
+                <div className="total-products-container">
+
+                </div>
+            </div>
+            <div className="row" id="charts-container">
+                <div className="product-sales-container">
+
+                </div>
+                <div className="ratings-chart-container">
+                    
+                </div>
+            </div>
+            <div className="row" id="latest-reviews-container">
+                <div className="latest-customer-reviews">
+
+                </div>
+            </div>
         </div>
     );
 }
